7e8110cae8d057e222946201efe5815e506a134c
TonyGo-1 <tonyyjgo@gmail.com>
2025-08-08 23:04:03 -0400

    Add files via upload

--- Biostatistics Back to Basics/Back to Basics for Biostatistics.R
+++ Biostatistics Back to Basics/Back to Basics for Biostatistics.R
@@ -32,6 +32,7 @@ leveneTest(TotalCholesterol_mgdl~factor(High_Chol),MasterData)
 ###Chapter 2: Visualizing Data ###
 
 ##Boxplots
+#This boxplot is comparing High Cholectrol Groups and Total Cholesterol groups
 MasterData %>%
   ggplot(aes(x=High_Chol,y=TotalCholesterol_mgdl,fill=High_Chol))+
   geom_boxplot()+
@@ -42,9 +43,30 @@ MasterData %>%
        title="Cholesterol Levels by High Cholesterol Status",
        fill="Group")+theme_minimal()
 
-##
+
 
 
 ###Chapter 3: Regression Analysis Assumptions ###
+#There are 4 assumptions that need to be met before conducting Linear Regression. 
+#LINE (Linearity, Independence, Normality and Equal Variance). Equal Variance can also be called homoscedasticity.
+
+##Normality
+#To test normality (in linear regression), we need to 
+
+#A Linear Regression is fit predicting total cholesterol from two Variables: Age & BMI.
+model<-lm(TotalCholesterol_mgdl ~ Age + BMI, data = MasterData)
+
+#plotting residuals against 
+residual_data<-data.frame(resid=residuals(model))
+qq<-ggplot(residual_data, aes(sample = resid)) +
+  stat_qq()+
+  stat_qq_line(color = "blue") +
+  theme_minimal() +
+  labs(title="Q-Q Plot: Regression Residuals")
+
+qq
+
+#It seems our qqplot shows heavy skewness.
+
 ###Chapter 4: Handling Missing Data ###
 
